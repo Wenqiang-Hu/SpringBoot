17f51762667241fc702cf05fe55b73f0ed04c96c
Wenqiang-Hu <63572326+Wenqiang-Hu@users.noreply.github.com>
2023-08-08 15:51:50 -0700

    Add: POST request to add new student to DB

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,10 +2,12 @@ package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -18,5 +20,15 @@ public class StudentService {
 
     public List<Student> getStudent(){
         return studentRepository.findAll();
+    }
+
+
+    public void registerStudent(Student student) {
+
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email has been taken");
+        }
+        studentRepository.save(student);
     }
 }
